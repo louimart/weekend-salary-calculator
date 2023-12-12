860a2088b0fdaaeabc03dba02b4dd9b3ceda2adc
Louis Martin <louimart@gmail.com>
2023-12-12 06:41:42 -0600

    css over-budget needs correcting to change the footer, not <p>. Clear form workign

--- script.js
+++ script.js
@@ -27,15 +27,43 @@ function submitEmployee(eventObj) {
         <td>${idInput.value}</td>
         <td>${titleInput.value}</td>
         <td>${annualSalaryInput.value}</td>
+        <td><button onclick="removeEmployee(event)">Delete</button></td>
     </tr>
     `;
     console.log(employee);
 
+    // calculating Total Monthly cost
     totalMonthly = (totalMonthly + Number(annualSalaryInput.value / 12));
     console.log(totalMonthly);
-    const totalSalary = document.querySelector('.totalMonthly');
-    if(totalSalary >= 20000){
-        totalSalary.innerHTML = `<p class="over-budget">Total Monthly: $${totalMonthly.toFixed(2)}</p>`  
+
+    // calculating total over budget amount for reference
+    totalOverBudget = totalMonthly - 20000;
+    console.log(totalOverBudget);
+
+    // using if-else statement to display total 
+    const totalMonthlyDisplay = document.querySelector('.totalMonthly');
+    if(totalMonthly >= 20000){
+        console.log('larger than 20,000')
+        totalMonthlyDisplay.innerHTML = `<p class="over-budget">Total Monthly: $${totalMonthly.toFixed(2)} (!!! $${totalOverBudget.toFixed(2)} over budget !!!)</p>`;
+    } else {
+        console.log('lower than 20,000')
+        totalMonthlyDisplay.innerHTML = `<p class="totalMonthly">Total Monthly: $${totalMonthly.toFixed(2)}</p>`;
     }
-    totalSalary.innerHTML = `<p class="totalMonthly">Total Monthly: $${totalMonthly.toFixed(2)}</p>`  
+    console.log(totalMonthlyDisplay);
+
+    clearForm();
+}
+
+function removeEmployee(event) {
+    employeeElement = event.target.parentElement.parentElement;
+    console.log(employeeElement);
+    employeeElement.remove();
+}
+
+function clearForm() {
+    firstNameInput.value = ''
+    lastNameInput.value = ''
+    idInput.value = ''
+    titleInput.value = ''
+    annualSalaryInput.value = ''
 }
